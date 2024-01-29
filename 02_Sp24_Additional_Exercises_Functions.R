905d2d9ff6113999c5d58ee8f2486f97774a9f5b
NagaVemprala <Naga.Vemprala@gmail.com>
2024-01-28 22:09:42 -0800

    Added new coding challenges

---
+++ 02_Sp24_Additional_Exercises_Functions.R
@@ -0,0 +1,36 @@
+
+#> Coding task 1: Summarize Species Data (iris dataset)
+#> Hint: Use summary function and interpret results. No need to create a separate function.
+#> Instead use the existing ones such as summary, str, head to understand the data. ----
+
+
+#> Coding task 2: Create a function that takes a species name as input and returns a summary of 
+#> its Sepal.Length, Sepal.Width, Petal.Length, and Petal.Width in the iris dataset, 
+#> including mean, median, standard deviation, and quartiles. Make sure that the 
+#> summary statistic is only for a single flower. Therefore, you should do a subset 
+#> option based on the type of flower passed as input argument to the function.
+#> ----
+
+#> Coding task 3: Find Outliers (mtcars dataset)
+#> Create a function that takes a column name from the mtcars dataset as input 
+#> and identifies outliers using interquartile range (IQR) method, returning a list of outlier values.
+#> Capture the return values into another variable and display it after creating the variable to hold 
+#> return values.
+
+
+#> Coding task 4: Visualize Correlations (mtcars dataset)
+#> Create a function that visualizes pairwise correlations between all numeric variables 
+#> in the mtcars dataset using separate scatter plots, with appropriate labels for x and y, and 
+#> plot colors. If possible explore the mfrow() option while creating plots in base R.
+
+#> Coding task 5: Generate Descriptive Statistics Report (any dataset)
+#> The dataset name should be passed as a text value in the argument. Create a function 
+#> that generates a comprehensive descriptive statistics report for any given dataset, 
+#> including measures of central tendency, dispersion, distribution shape, and missing values.
+#> Hint: You can use the get() function to treat the string value as a variable name.
+#> Pass the dataset name in lower case as input to the function as a string, so that it can
+#> be extracted using a get("string passed") to use the value as a dataset itself inside 
+#> function body.
+
+
+
